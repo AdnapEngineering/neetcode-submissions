e505ff4285eac430d6c2e42f3414c956e83520d2
AdnapEngineering <54551478+AdnapEngineering@users.noreply.github.com>
2026-08-26 16:06:13 -0700

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # convert prerequisites into AdjacencyMap
+        adjMap = {i:[] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            adjMap[crs].append(pre)
+
+        # visiting set to detect loops:
+        visiting = set()
+
+        def dfs(crs):
+            if crs in visiting: return False # base case, cycle.
+            if adjMap[crs] == []: return True # Base case - safe/no-prereqs
+            visiting.add(crs)
+            for pre in adjMap[crs]:
+                if not dfs(pre): return False
+            visiting.remove(crs) # crs is safe
+            adjMap[crs] = []
+            return True
+
+        for crs in range(numCourses):
+            if not dfs(crs) : return False
+        return True    
